551a440d3d1fa9b0ae9bfa85ce6002f13b49922e
JiaweiLing <jiaweiling1997@outlook.com>
2018-05-18 17:19:21 +0800

    add structure and hash table

--- Lab2/semantic.c
+++ Lab2/semantic.c
@@ -8,3 +8,38 @@ void check_semantic(tree *root)
 {
 
 }
+
+unsigned hash_table(char *name)
+{
+	unsigned int val = 0, i;
+	for (; *name; ++name)
+	{
+		val = (val << 2) + *name;
+		if (i = val & ~0x3fff) val = (val ^ (i >> 12)) & 0x3fff;
+	}
+	return val;
+}
+
+void init_hash()
+{
+	int i;
+	for (i = 0; i < hash_size; i++)
+		symbolhash[i] = NULL;
+}
+
+void insert_symbol_table(symbolpt node)
+{
+	unsigned index = hash_table(node->name);
+	if (symbolhash[index] == NULL)
+	{
+		symbolhash[index] = node;
+		node->next = NULL;
+		return 0;
+	}
+	else
+	{
+		node->next = symbolhash[index];
+		symbolhash[index] = node;
+		return 1;
+	}
+}
--- Lab2/semantic.h
+++ Lab2/semantic.h
@@ -1,5 +1,18 @@
+#include "tree.h"
+
 typedef struct TYPE* Type;
 typedef struct FIELDLIST* FieldList;
+typedef struct FUNPARALIST* FunParaList;
+typedef struct SymbolTableNode* symbolpt;
+
+struct SymbolTableNode
+{
+	char *name;
+	Type type;
+	int scope;
+	symbolpt next;
+	symbolpt scope_next;
+};
 
 struct TYPE
 {
@@ -13,7 +26,20 @@ struct TYPE
 			int size;
 		} Array;
 		FieldList Structure;
+		struct
+		{
+			int num_para;
+			Type return_type;
+			FunParaList fpl;
+		} func;
 	} Union;
+};
+
+struct FUNPARALIST
+{
+	char *name;
+	Type type;
+	FunParaList next;
 };
 
 struct FIELDLIST
@@ -21,4 +47,12 @@ struct FIELDLIST
 	char *name;
 	Type type;
 	FieldList next;
-}
+};
+
+#define hash_size 0x3fff
+symbolpt symbolhash[hash_size];
+void check_semantic(tree *root);
+unsigned hash(char *name);
+void init_hash();
+void insert_symbol_table(symbolpt node);
+
